5ef48a339a3a60c3d19327ff3d5989ecc07d12cf
Gabriel DiFiore <difioregabe@gmail.com>
2023-09-14 17:42:17 -0400

    Fix build errors with new file locations

--- Interfaces/IPlayer.cs
+++ Interfaces/IPlayer.cs
@@ -5,7 +5,7 @@ using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LegendOfZelda
+namespace LegendOfZelda.Interfaces
 {
     public interface IPlayer : iUpdateable, iDrawable
     {
