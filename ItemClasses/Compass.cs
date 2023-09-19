806f8e2075c67046f9f10ea55e580fbcc79b2b3e
SimonL9889 <113265728+SimonL9889@users.noreply.github.com>
2023-09-19 17:07:25 -0400

    Add item classes

---
+++ ItemClasses/Compass.cs
@@ -0,0 +1,29 @@
+﻿using LegendOfZelda;
+using LegendOfZelda.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+namespace LegendOfZelda
+{
+    public class Compass : IItem
+    {
+        protected Texture2D texture;
+        protected SpriteBatch spriteBatch;
+        protected Game1 game1;
+        public Vector2 position { get; protected set; }
+
+        public Compass(Texture2D texture, Vector2 position)
+        {
+            this.texture = texture;
+            this.position = position;
+            game1 = Game1.instance;
+            spriteBatch = game1._spriteBatch;
+        }
+
+        public void Draw()
+        {
+            spriteBatch.Draw(texture, position, Color.White);
+        }
+    }
+}
+
