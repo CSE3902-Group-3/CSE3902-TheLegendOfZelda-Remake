cf7c5549efa3900f128df06b11b93e98a64bd3e0
turtenguin <turtenguin@gmail.com>
2023-09-21 11:50:15 -0400

    Create IPlayerProjectile.cs

---
+++ Interfaces/IPlayerProjectile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegendOfZelda.Interfaces
+{
+    public interface IPlayerProjectile
+    {
+        public enum Direction { left, right, up, down };
+        public void Create(Vector2 position, Direction direction);
+    }
+}
