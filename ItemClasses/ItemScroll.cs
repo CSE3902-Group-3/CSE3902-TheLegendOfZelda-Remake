89b5f4de9ea1f7e2986379fd505c5b1b76c1e33e
Laidawosai <huzhengyibb@hotmail.com>
2023-10-01 16:10:56 -0400

    Reset command complete & Reset method for enemyCycler added $ Reset for ItemScroll bug fixed
    After merging into the main, the reset should able to reset the game correctly.

--- ItemClasses/ItemScroll.cs
+++ ItemClasses/ItemScroll.cs
@@ -79,7 +79,8 @@ namespace LegendOfZelda
         public void Reset()
         {
             itemCollection[i].Remove();
-            itemCollection[0].Show();
+            i = 0;
+            itemCollection[i].Show();
         }
 
     }
