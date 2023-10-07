0c14975acabeceead5179e9973eb7991eac2e357
SimonL9889 <113265728+SimonL9889@users.noreply.github.com>
2023-10-07 15:44:44 -0400

    Create Inventory.cs

---
+++ Inventory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LegendOfZelda
+{
+	public class Inventory
+	{
+        private Dictionary<IItem, int> inventory = new Dictionary<IItem, int>();
+
+        public void AddItem(IItem item)
+        {
+            if (inventory.ContainsKey(item))
+            {
+                inventory[item]++;
+            }
+            else
+            {
+                inventory[item] = 1;
+            }
+        }
+
+        public void RemoveItem(IItem item)
+        {
+            if (inventory.ContainsKey(item))
+            {
+                inventory[item]--;
+
+            }
+            else if (inventory[item] <= 0)
+            {
+                inventory.Remove(item);
+            }
+        }
+
+        public int GetQuantity(IItem item)
+        {
+            int itemAmount = 0;
+            if (inventory.ContainsKey(item))
+            {
+                itemAmount = inventory[item];
+            }
+
+            return itemAmount;
+        }
+    }
+}
+
