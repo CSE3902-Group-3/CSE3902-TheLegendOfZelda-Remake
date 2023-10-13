34b38c5ca51c03c14724b866c6108cfb84a09267
turtenguin <turtenguin@gmail.com>
2023-10-13 11:47:33 -0400

    Finish boomerang collision
    Also updated projectiletest to show boomerangs

--- Projectiles/ProjectileTest.cs
+++ Projectiles/ProjectileTest.cs
@@ -22,33 +22,40 @@ namespace LegendOfZelda
             {
                 case 0:
                     new ArrowProjectile(pos, Direction.up);
+                    new BoomerangProjectile(pos, new Vector2(0, 1), Game1.getInstance().link);
                     break;
                 case 1:
                     new ArrowProjectile(pos, Direction.right);
+                    new BoomerangProjectile(pos, new Vector2(1, 1), Game1.getInstance().link);
                     break;
                 case 2:
+                    new BoomerangProjectile(pos, new Vector2(1, 0), Game1.getInstance().link);
                     new ArrowProjectile(pos, Direction.down);
                     break;
                 case 3:
                     new ArrowProjectile(pos, Direction.left);
+                    new BoomerangProjectile(pos, new Vector2(1, -1), Game1.getInstance().link);
                     break;
                 case 4:
+                    new BoomerangProjectile(pos, new Vector2(0, -1), Game1.getInstance().link);
                     new BombProjectile(pos);
                     break;
                 case 5:
-                    new BoomerangProjectile(pos, new Vector2(1, 1), Game1.getInstance().link);
+                    new BoomerangProjectile(pos, new Vector2(-1, -1), Game1.getInstance().link);
                     break;
                 case 6:
-                    new FireProjectile(pos, Direction.up);
+                    //new FireProjectile(pos, Direction.up);
+                    new BoomerangProjectile(pos, new Vector2(-1, 0), Game1.getInstance().link);
                     break;
                 case 7:
-                    new FireProjectile(pos, Direction.right);
+                    //new FireProjectile(pos, Direction.right);
+                    new BoomerangProjectile(pos, new Vector2(-1, 1), Game1.getInstance().link);
                     break;
                 case 8:
-                    new FireProjectile(pos, Direction.down);
+                    //new FireProjectile(pos, Direction.down);
                     break;
                 case 9:
-                    new FireProjectile(pos, Direction.left);
+                    //new FireProjectile(pos, Direction.left);
                     break;
             }
 
