ebe33ab4fcbb6c5ffebc17201d037eb11f6973d6
EthanGlenwright775 <112444137+EthanGlenwright775@users.noreply.github.com>
2023-11-02 11:32:21 -0400

    triforce pickup trigger winState

--- ItemClasses/Triforce.cs
+++ ItemClasses/Triforce.cs
@@ -36,6 +36,7 @@ namespace LegendOfZelda
         {
             triforce.UnregisterSprite();
             collider.Active = false;
+            GameState.GetInstance().SwitchState(new WinningState());
             return this;
         }
 
