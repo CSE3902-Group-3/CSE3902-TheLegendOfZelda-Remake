3c49d8a1b6b78662d761277c54ad734651d1798c
SimonL9889 <113265728+SimonL9889@users.noreply.github.com>
2023-11-16 14:29:12 -0500

    Code Quality Update

--- SpecialScreens/GameOverScreenDrawables/GameOverText.cs
+++ SpecialScreens/GameOverScreenDrawables/GameOverText.cs
@@ -8,22 +8,12 @@ namespace LegendOfZelda
 	{
         private Game1 game;
         private GraphicsDevice graphicsDevice;
-        private SpriteBatch spriteBatch;
         private int CameraXPos;
         private int CameraYPos;
         private int StartXPos;
         private int StartYPos;
         private int letterWidth;
         private LetterFactory letterFactory;
-        private AnimatedSprite G;
-        private AnimatedSprite A;
-        private AnimatedSprite M;
-        private AnimatedSprite E;
-        private AnimatedSprite space;
-        private AnimatedSprite O;
-        private AnimatedSprite V;
-        private AnimatedSprite E2;
-        private AnimatedSprite R;
 
         private List<AnimatedSprite> text;
 
@@ -31,26 +21,24 @@ namespace LegendOfZelda
 		{
             game = Game1.getInstance();
             graphicsDevice = game.GraphicsDevice;
-            spriteBatch = game._spriteBatch;
             CameraXPos = (int)GameState.CameraController.gameOverCamera.worldPos.X;
             CameraYPos = (int)GameState.CameraController.gameOverCamera.worldPos.Y;
             StartXPos = CameraXPos + (graphicsDevice.Viewport.Width * 2 / 5);
             StartYPos = CameraYPos + (graphicsDevice.Viewport.Height / 2);
             letterWidth = 30;
             letterFactory = LetterFactory.GetInstance();
-            G = letterFactory.GetLetterSprite('G');
-            A = letterFactory.GetLetterSprite('A');
-            M = letterFactory.GetLetterSprite('M');
-            E = letterFactory.GetLetterSprite('E');
-            space = letterFactory.GetBlankSprite();
-            O = letterFactory.GetLetterSprite('O');
-            V = letterFactory.GetLetterSprite('V');
-            E2 = letterFactory.GetLetterSprite('E');
-            R = letterFactory.GetLetterSprite('R');
 
             text = new List<AnimatedSprite>()
             {
-                G, A, M, E, space, O, V, E2, R
+                letterFactory.GetLetterSprite('G'),
+                letterFactory.GetLetterSprite('A'),
+                letterFactory.GetLetterSprite('M'),
+                letterFactory.GetLetterSprite('E'),
+                letterFactory.GetBlankSprite(),
+                letterFactory.GetLetterSprite('O'),
+                letterFactory.GetLetterSprite('V'),
+                letterFactory.GetLetterSprite('E'),
+                letterFactory.GetLetterSprite('R')
             };
         }
 
