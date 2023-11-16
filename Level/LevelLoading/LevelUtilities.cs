b4cd0dc5c6767672d6cce2ae9b2aff62093a8a5a
EthanGlenwright775 <112444137+EthanGlenwright775@users.noreply.github.com>
2023-11-16 12:42:57 -0500

    level utils

--- Level/LevelLoading/LevelUtilities.cs
+++ Level/LevelLoading/LevelUtilities.cs
@@ -4,15 +4,15 @@ namespace LegendOfZelda
 {
     internal class LevelUtilities
     {
-        public static int RoomWidth = 1024;
-        public static int RoomHeight = 704;
-        Vector2 pos = new Vector2(room.RoomXLocation + WallThickness + Scale * mapElement.XLocation, room.RoomYLocation + YOffset + Scale * mapElement.YLocation);
-        
-        public Vector2 CalculateElementPositionWithOffset(Room room, MapElement mapElement)
+        public static int RoomWidth { get; } = 1024;
+        public static int RoomHeight { get; } = 704;
+        public static int GridUnitSize { get; } = SpriteFactory.getInstance().scale * 16;
+        public static int YMenuOffset { get; } = 192;
+        public static Vector2 CalculateElementPositionWithOffset(Room room, MapElement mapElement)
         {
-
+            return new Vector2(room.RoomXLocation * RoomWidth + WallThickness + GridUnitSize * mapElement.XLocation, room.RoomYLocation * RoomHeight * -1 + YOffset + GridUnitSize * mapElement.YLocation);
         }
-        public Vector2 CalculateElementPositionWithNoOffset(Room room, MapElement mapElement)
+        public static Vector2 CalculateElementPositionWithNoOffset(Room room, MapElement mapElement)
         {
 
         }
