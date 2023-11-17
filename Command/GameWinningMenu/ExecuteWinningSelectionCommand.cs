cd044235a82b2f49740fc3682de21cfe687eb356
SimonL9889 <113265728+SimonL9889@users.noreply.github.com>
2023-11-17 12:17:27 -0500

    Add Controller for selector

---
+++ Command/GameWinningMenu/ExecuteWinningSelectionCommand.cs
@@ -0,0 +1,18 @@
+﻿namespace LegendOfZelda
+{
+    public class ExecuteWinningSelectionCommand : ICommands
+    {
+        private WinningSelector selector;
+
+        public ExecuteWinningSelectionCommand()
+        {
+            selector = WinningState.selector;
+        }
+
+        public void Execute()
+        {
+            selector.ExecuteSelection();
+        }
+    }
+}
+
