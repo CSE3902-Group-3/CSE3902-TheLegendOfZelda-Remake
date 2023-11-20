06ceb4805a6eb2da5ea2de8556f8eaf8e5a87b97
turtenguin <turtenguin@gmail.com>
2023-11-19 23:58:49 -0500

    Add pallet sampler, Link, background, enemies

---
+++ Content/pallet.fx
@@ -0,0 +1,40 @@
+﻿//Standard shader (no effects) taken from tutorial: https://community.monogame.net/t/writing-your-own-2d-pixel-shader-in-monogame-for-absolute-beginners/10883
+
+#if OPENGL
+#define SV_POSITION POSITION
+#define VS_SHADERMODEL vs_3_0
+#define PS_SHADERMODEL ps_3_0
+#else
+#define VS_SHADERMODEL vs_4_0_level_9_1
+#define PS_SHADERMODEL ps_4_0_level_9_1
+#endif
+
+Texture2D SpriteTexture;
+float4 Pallet[16];
+
+sampler2D SpriteTextureSampler = sampler_state
+{
+    Texture = <SpriteTexture>;
+};
+
+struct VertexShaderOutput
+{
+    float4 Position : SV_POSITION;
+    float4 Color : COLOR0;
+    float2 TextureCoordinates : TEXCOORD0;
+};
+
+float4 MainPS(VertexShaderOutput input) : COLOR
+{
+    float4 index = tex2D(SpriteTextureSampler, input.TextureCoordinates);
+    float4 sampledColor = Pallet[index.r * 256] * input.Color * float4(1, 1, 1, index.a);
+    return sampledColor;
+}
+
+technique SpriteDrawing
+{
+    pass P0
+    {
+        PixelShader = compile PS_SHADERMODEL MainPS();
+    }
+};
--- Graphics/ShaderHolder.cs
+++ Graphics/ShaderHolder.cs
@@ -1,10 +1,7 @@
-﻿using Microsoft.Xna.Framework.Content;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LegendOfZelda
 {
@@ -13,16 +10,48 @@ namespace LegendOfZelda
         public static readonly bool ShadersOn = true;
         private static bool loaded = false;
         public static Effect normalShader;
+
+        public static Effect standard;
         public static Effect flash1;
         public static Effect flash2;
         public static Effect blink;
 
+        public static Effect standardPallet;
+
+        private static Texture2D pallet0;
+
         public static void LoadShaders(ContentManager content)
         {
-            normalShader = content.Load<Effect>("normal");
+            standard = content.Load<Effect>("normal");
             flash1 = content.Load<Effect>("flash1");
             flash2 = content.Load<Effect>("flash2");
             blink = content.Load<Effect>("blink");
+            standardPallet = content.Load<Effect>("pallet");
+
+            Vector4[] pallet = new Vector4[16]
+            {
+                new Vector4(0, 0, 0, 1),
+                new Vector4(128/252f, 208/252f, 16/252f, 1),
+                new Vector4(200/252f, 76/252f, 12/252f, 1),
+                new Vector4(252/252f, 152/252f, 56/252f, 1),
+                new Vector4(1, 1, 1, 1),
+                new Vector4(92/252f, 148/252f, 1, 1),
+                new Vector4(0, 0, 168/252f, 1),
+                new Vector4(1, 0, 0, 1),
+                new Vector4(24/252f, 60/252f, 92/252f, 1),
+                new Vector4(0, 232/252f, 216/252f, 1),
+                new Vector4(0, 128/252f, 136/252f, 1),
+                new Vector4(188/252f, 188/252f, 188/252f, 1),
+                new Vector4(116/252f, 116/252f, 116/252f, 1),
+                new Vector4(0, 80/252f, 0, 1),
+                new Vector4(1, 0, 0, 1),
+                new Vector4(1, 0, 0, 1)
+            };
+
+            standardPallet.Parameters["Pallet"].SetValue(pallet);
+
+            normalShader = standardPallet;
+
             loaded = true;
         }
     }
