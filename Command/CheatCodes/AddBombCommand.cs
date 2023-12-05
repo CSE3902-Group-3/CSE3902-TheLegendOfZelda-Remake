1062f7da6b651e7cb8aa7dd649a33d4bea1e785c
SimonL9889 <113265728+SimonL9889@users.noreply.github.com>
2023-12-04 20:33:00 -0500

    Add Bomb

---
+++ Command/CheatCodes/AddBombCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace LegendOfZelda
+{
+    public class AddBombCommand : ICommands
+    {
+        private AddBomb cheatCode;
+        public AddBombCommand()
+        {
+            cheatCode = new AddBomb();
+        }
+
+        public void Execute()
+        {
+            cheatCode.Execute();
+        }
+    }
+}
+
