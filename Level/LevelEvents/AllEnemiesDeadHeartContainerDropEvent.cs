6186cb8f3885d757e273d77a01178abc2a0dda64
EthanGlenwright775 <112444137+EthanGlenwright775@users.noreply.github.com>
2023-12-06 13:45:29 -0500

    events

---
+++ Level/LevelEvents/AllEnemiesDeadHeartContainerDropEvent.cs
@@ -0,0 +1,33 @@
+﻿using LegendOfZelda.Interfaces;
+using Microsoft.Xna.Framework;
+
+namespace LegendOfZelda
+{
+    internal class AllEnemiesDeadHeartContainerDropEvent : ILevelEvent
+    {
+        private Vector2 Position;
+        public AllEnemiesDeadHeartContainerDropEvent(Vector2 position)
+        {
+            Position = position;
+            LevelManager.AddUpdateable(this);
+        }
+        private void ConditionSuccess()
+        {
+            IItem heartContainer = new HeartContainer(Position);
+            heartContainer.Show();
+            SoundFactory.PlaySound(SoundFactory.getInstance().KeyAppear);
+            LevelManager.RemoveUpdateable(this);
+        }
+        public void CheckCondition()
+        {
+            if (!LevelManager.CurrentLevelRoom.EnemiesInRoom()) 
+            {
+                ConditionSuccess();
+            }
+        }
+        public void Update(GameTime gameTime)
+        {
+            CheckCondition();
+        }
+    }
+}
